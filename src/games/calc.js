b1d11048c54c9de3ac57657fa71b1dd65e61e75d
gjoobis <marshmire@gmail.com>
2023-01-31 17:45:50 +0300

    demagick

--- src/games/calc.js
+++ src/games/calc.js
@@ -7,8 +7,8 @@ const maxNumber = 10;
 const getGameData = () => {
   const a = getRandomNumber(minNumber, maxNumber);
   const b = getRandomNumber(minNumber, a);
-  const i = getRandomNumber(0, 3);
   const operators = ['+', '-', '*'];
+  const i = getRandomNumber(0, operators.length);
   const operations = [a + b, a - b, a * b];
   const task = `${a} ${operators[i]} ${b}`;
   const correctAnswer = `${operations[i]}`;
