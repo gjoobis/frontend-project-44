bb276a94c93b5c5e8e17c375baa5ef1f444634be
gjoobis <marshmire@gmail.com>
2023-01-31 12:41:26 +0300

    step six: add calc game

---
+++ src/games/calc.js
@@ -0,0 +1,23 @@
+import gameBasics from '../index.js';
+import getRandomNumber from '../random-number.js';
+
+const minNumber = 1;
+const maxNumber = 10;
+
+const getGameData = () => {
+  const a = getRandomNumber(minNumber, maxNumber);
+  const b = getRandomNumber(minNumber, a);
+  const i = getRandomNumber(0, 3);
+  const operators = ['+', '-', '*'];
+  const operations = [a + b, a - b, a * b];
+  const task = `${a} ${operators[i]} ${b}`;
+  const correctAnswer = `${operations[i]}`;
+  return [task, correctAnswer];
+};
+
+const calcGame = () => {
+  const rule = 'What is the result of the expression?';
+  gameBasics(rule, getGameData);
+};
+
+export default calcGame;
